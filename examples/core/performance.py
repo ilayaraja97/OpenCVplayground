058e920410fb8032b9a54685113d8e1c82332c11
ilayaraja97 <ilayaraja97@gmail.com>
2020-09-29 21:44:11 +0530

    add useOptimized()

--- examples/core/performance.py
+++ examples/core/performance.py
@@ -1,4 +1,6 @@
+#%% imports
 import cv2 as cv
+#%% getTickCount()
 img = cv.imread('messi5.jpg')
 e1 = cv.getTickCount()
 for i in range(5,49,2):
@@ -6,3 +8,6 @@ for i in range(5,49,2):
 e2 = cv.getTickCount()
 t = (e2 - e1)/cv.getTickFrequency()
 print( t )
+#%% useOptimized()
+print(cv.useOptimized())
+# %%
