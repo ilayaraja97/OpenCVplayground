38dedeffc2108319e286aaa6398a2cf84ff7f6bc
ilayaraja97 <ilayaraja97@gmail.com>
2020-10-08 22:06:38 +0530

    matrix subtraction with threshold method

---
+++ mini_projects/moving_obj_det.py
@@ -0,0 +1,26 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture("http://192.168.1.120:4747/video")
+cv.namedWindow('image')
+
+_,prev_frame = cap.read()
+prev_frame=cv.cvtColor(prev_frame,cv.COLOR_BGR2GRAY)
+
+while(1):
+    # Take each frame
+    _, img = cap.read()
+    if img is None:
+        break
+    # img=cv.resize(img,(800,600))
+    img=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    mask=np.absolute(img-prev_frame)
+    mask[mask<250]=0
+    # print(mask)
+    # res=cv.bitwise_and(img,img,mask=mask)
+    cv.imshow('image',mask)
+    prev_frame=img
+    k = cv.waitKey(20) & 0xFF
+    if k == 27:
+        break
+cv.destroyAllWindows()
